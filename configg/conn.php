e92270ad6086dcf28f0d3f59919cc2c145b6a148
USAMA MEMON <usama15memon@gmail.com>
2025-05-05 21:03:52 +0500

    Add files via upload

---
+++ configg/conn.php
@@ -0,0 +1,13 @@
+<?php 
+    $servername =  "localhost" ;
+    $username   =  "root" ;
+    $password   =  ""           ;
+    $dbname     =  "zoyan2" ;
+
+    $conn =mysqli_connect($servername , $username , $password ,$dbname);
+
+    if(!$conn){
+        die("Connection Has Been Failed");
+    }
+
+?>
